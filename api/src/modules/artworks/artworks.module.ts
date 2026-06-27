8db13516dd1d99a064204324da7c2dcbcef6e58c
ahmataaffadine99 <ahmataffadine82@gmail.com>
2026-06-27 11:52:17 +0200

    feat(artworks): configure module with typeorm

--- api/src/modules/artworks/artworks.module.ts
+++ api/src/modules/artworks/artworks.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArtworksService } from './artworks.service';
 import { ArtworksController } from './artworks.controller';
+import { Artwork } from './entities/artwork.entity';
+import { Artist } from '../artists/entities/artist.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Artwork, Artist])],
   controllers: [ArtworksController],
   providers: [ArtworksService],
 })
