60ffc0b0a332e7718df6105f928c77df11e15974
ahmataaffadine99 <ahmataffadine82@gmail.com>
2026-06-27 11:41:10 +0200

    feat(artists): add create and update DTOs

--- api/src/modules/artists/dto/create-artist.dto.ts
+++ api/src/modules/artists/dto/create-artist.dto.ts
@@ -1,1 +1,19 @@
-export class CreateArtistDto {}
+import { IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateArtistDto {
+  @ApiPropertyOptional({ description: 'Biographie détaillée de l’artiste' })
+  @IsOptional()
+  @IsString()
+  biography?: string;
+
+  @ApiProperty({ example: 'Française', description: 'Nationalité de l’artiste' })
+  @IsNotEmpty()
+  @IsString()
+  nationality: string;
+
+  @ApiPropertyOptional({ example: 'https://monportfolio.com', description: 'Lien vers un portfolio externe' })
+  @IsOptional()
+  @IsUrl()
+  portfolioUrl?: string;
+}
