a988f60b96ba5026416b7651e045067d1cdb8eac
elwardi <a.elwardi98@gmail.com>
2026-07-14 13:11:19 +0200

    integrate status history recording in exhibitions service on loan and return

--- api/src/modules/exhibitions/exhibitions.module.ts
+++ api/src/modules/exhibitions/exhibitions.module.ts
@@ -5,9 +5,10 @@ import { ExhibitionsController } from './exhibitions.controller';
 import { Exhibition } from './entities/exhibition.entity';
 import { Loan } from './entities/loan.entity';
 import { Artwork } from '../artworks/entities/artwork.entity';
+import { ArtworksModule } from '../artworks/artworks.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Exhibition, Loan, Artwork])],
+  imports: [TypeOrmModule.forFeature([Exhibition, Loan, Artwork]), ArtworksModule],
   controllers: [ExhibitionsController],
   providers: [ExhibitionsService],
   exports: [ExhibitionsService],
