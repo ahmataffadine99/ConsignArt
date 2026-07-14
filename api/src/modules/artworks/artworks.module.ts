c79eafea056ec3c3b685df82ff8549e760d6c870
elwardi <a.elwardi98@gmail.com>
2026-07-14 12:44:11 +0200

    register ArtworkStatusHistory entity and service in ArtworksModule

--- api/src/modules/artworks/artworks.module.ts
+++ api/src/modules/artworks/artworks.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArtworksService } from './artworks.service';
 import { ArtworksController } from './artworks.controller';
 import { Artwork } from './entities/artwork.entity';
+import { ArtworkStatusHistory } from './entities/artwork-status-history.entity';
 import { Artist } from '../artists/entities/artist.entity';
+import { ArtworkStatusHistoryService } from './artwork-status-history.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Artwork, Artist])],
+  imports: [TypeOrmModule.forFeature([Artwork, ArtworkStatusHistory, Artist])],
   controllers: [ArtworksController],
-  providers: [ArtworksService],
-  exports: [ArtworksService],
+  providers: [ArtworksService, ArtworkStatusHistoryService],
+  exports: [ArtworksService, ArtworkStatusHistoryService],
 })
 export class ArtworksModule {}
