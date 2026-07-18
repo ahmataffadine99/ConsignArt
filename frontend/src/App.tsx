43c64849f8de3518216ff23dfe2671bbd7fa0edb
ahmataaffadine99 <ahmataffadine82@gmail.com>
2026-07-18 12:26:40 +0200

    feat: init react vite frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>ConsignArt</h1>
+    </div>
+  );
+}
+
+export default App;
